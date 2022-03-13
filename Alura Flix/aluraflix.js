393ad6e1970d5914c140f16da27d459ed757c95d
Bruno Raphael <brunoraphaell99@gmail.com>
2022-03-13 14:45:50 -0300

    desafio 2 check

--- Alura Flix/aluraflix.js
+++ Alura Flix/aluraflix.js
@@ -1,15 +1,35 @@
 // Estudar no HTML telas responsivos.
 // X Desafio 1: Colocar o link para quando clicar na imagem ir para a página do IMDB.
-// Desafio 2: Colocar a condição para não adicionar filmes repetidos.
+// X Desafio 2: Colocar a condição para não adicionar filmes repetidos.
 // Desafio 3: Colocar um botão que quando coloca o link da imagem a capa do filme aparece. 
 
-// href_IMDB, link_imagem
-var listaFilmes = new Map([
-    ["https://www.imdb.com/title/tt8079248/", "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg"],
-    ["https://www.imdb.com/title/tt2543164/?ref_=fn_al_tt_1", "https://images-na.ssl-images-amazon.com/images/I/91382RVTr3L.jpg"],
-    ["https://www.imdb.com/title/tt0332379/?ref_=nv_sr_srsg_0", "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg"]
-])
-
-for (var [key, value] of listaFilmes) {
-    document.write("<a href='" + key + "'>" + "<img src='" + value + "'>" + "</a>")
+function removeDuplicates(arrayFilmes) {
+    for (var i = 0; i < arrayFilmes.length; i++) {
+        for (var j = i + 1; j < arrayFilmes.length; j++) {
+            if (arrayFilmes[i].href === arrayFilmes[j].href) {
+                arrayFilmes.splice(j, 1);
+            }
+        }
+    }
+    return arrayFilmes;
 }
+
+const filmes = [
+    {href: "https://www.imdb.com/title/tt8079248/", imagem: "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg"},
+    {href: "https://www.imdb.com/title/tt2543164/?ref_=fn_al_tt_1", imagem: "https://images-na.ssl-images-amazon.com/images/I/91382RVTr3L.jpg"},
+    {href: "https://www.imdb.com/title/tt8079248/", imagem: "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg"},
+    {href: "https://www.imdb.com/title/tt0332379/?ref_=nv_sr_srsg_0", imagem: "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg"},
+    {href: "https://www.imdb.com/title/tt0332379/?ref_=nv_sr_srsg_0", imagem: "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg"},
+    {href: "https://www.imdb.com/title/tt2543164/?ref_=fn_al_tt_1", imagem: "https://images-na.ssl-images-amazon.com/images/I/91382RVTr3L.jpg"},
+    {href: "https://www.imdb.com/title/tt8079248/", imagem: "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg"}
+]
+
+
+var listaFilmes = removeDuplicates(filmes)
+
+console.log(filmes)
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<a href='" + listaFilmes[i].href + "'>" + "<img src='" + listaFilmes[i].imagem + "'>" + "</a>")
+}
+
